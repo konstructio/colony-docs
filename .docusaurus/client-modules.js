c57028cc0b10dd45afa4f26dc96c6ca354fee405
Frédéric Harper <hi@fred.dev>
2024-07-03 17:32:12 -0400

    feat: create an empty Docusaurus site for Colony (#3)

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/fharper/Documents/code/k1/colony-docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/fharper/Documents/code/k1/colony-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/fharper/Documents/code/k1/colony-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/fharper/Documents/code/k1/colony-docs/src/css/custom.css"),
+];
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# Colony Docs
+
+Yep
